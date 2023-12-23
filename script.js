c183f1db277084fb42d74c866f1cd88444191c63
Kirasakuragi <146707116+Kirasakuragi@users.noreply.github.com>
2023-12-24 00:42:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Aaaaa, Dede sukaa nesia jugaaa";
+    question.innerHTML = "Aaaaa, Bibim sukaa nesia jugaaa";
     gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
 
